0b7f9f8d39c1afdca6191b85c1f53c71c4283168
Yesset <poxerface5@gmail.com>
2024-11-27 13:37:08 +0500

    feat: prisma adapter

--- auth.ts
+++ auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "./lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [],
+  adapter: PrismaAdapter(prisma),
 });
