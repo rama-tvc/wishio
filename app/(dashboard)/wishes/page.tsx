aa12833496fc8adaba639aeb9c10545981336f24
Dcox <timur@rama.gg>
2024-12-06 16:27:57 +0500

    feat: add wishes list with base style

--- app/(dashboard)/wishes/page.tsx
+++ app/(dashboard)/wishes/page.tsx
@@ -1,5 +1,31 @@
 export default function WishPage(){
     return(
-        <p>TUT WISHES</p>
+        <div className="flex h-screen bg-gray-100">
+            <aside className="w-64 bg-white border-r shadow-sm">
+                <div className="h-16 flex items-center justify-center border-b">
+                    <h1 className="text-xl font-bold">WishList</h1>
+                </div>
+                <nav className="mt-4">
+                    <ul className="space-y-2 px-4">
+                        <li>
+                            <a href="#" className="flex items-center p-2 text-gray-600 hover:bg-gray-200 rounded">Главная страница</a>
+                        </li>
+                        <li>
+                        <a href="#" className="flex items-center p-2 text-gray-600 hover:bg-gray-200 rounded">Списки желаний</a>
+                        </li>
+                        <li>
+                        <a href="#" className="flex items-center p-2 text-gray-600 hover:bg-gray-200 rounded">Настройка профиля</a>
+                        </li>
+                    </ul>
+                </nav>
+            </aside>
+
+
+            <div className="flex-1 flex flex-col">
+                <header className="h-16 bg-white border-b ">
+                    
+                </header>
+            </div>
+        </div>
     )
 }
