023494da84776e1d90bc4e371374cd17a7d38100
Yesset <poxerface5@gmail.com>
2024-12-08 12:51:58 +0500

    chore: rename

--- actions/get-wishlists.ts
+++ actions/get-wishlists.ts
@@ -1,11 +1,18 @@
 "use server";
 
+import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
 
-export const getWishlists = async (userId: string) => {
+export const getUserWishlists = async () => {
+  const session = await auth();
+
+  if (!session || !session.user || !session.user.id) {
+    throw new Error("Unauthorized");
+  }
+
   const userWishlists = await prisma.wishlist.findMany({
     where: {
-      userId: userId,
+      userId: session.user.id,
     },
   });
 
--- app/(dashboard)/lists/page.tsx
+++ app/(dashboard)/lists/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createWishlist } from "@/actions/create-wishlist";
-import { getWishlists } from "@/actions/get-wishlists";
+import { getUserWishlists } from "@/actions/get-wishlists";
 import { Wishlist } from "@prisma/client";
 import { FormEventHandler, useEffect, useState } from "react";
 
@@ -9,8 +9,7 @@ export default function WishlistsPage() {
   const [wishlists, setWishlists] = useState<Wishlist[]>([]);
 
   const fetchUserWishlists = async () => {
-    const response = await fetch("/api/wishlists?userId=1");
-    const data = await response.json();
+    const data = await getUserWishlists();
 
     setWishlists(data);
   };
