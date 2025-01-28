26b44f20a84fedcbf3cce2e777eb15b374da7ff1
Baharamahan <bahaye@rama.gg>
2025-01-28 18:01:37 +0500

    fix: types fix

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -1,16 +1,12 @@
-import NextAuth from "next-auth";
+import "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string; // Add id to the user object
+      id: string;
       name?: string | null;
       email?: string | null;
       image?: string | null;
     };
-  }
-
-  interface JWT {
-    id: string; // Include id in JWT token
   }
 }
